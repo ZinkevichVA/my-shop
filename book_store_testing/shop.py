97b8d07f62abe5ee54e95cb52af2419fa1b8fa1c
ZinkevichVA <zinkevichva@gmail.com>
2022-12-07 21:48:06 +0300

    Test 8 "Price check in basket", Test 9 "Work in basket", Test 10 "Purchase of goods" are passed.

--- book_store_testing/shop.py
+++ book_store_testing/shop.py
@@ -173,39 +173,182 @@ time.sleep(1)
 driver.quit()
 '''
 #Tast 8: Price check in basket
+'''
+import time
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(5)
+wait = WebDriverWait(driver, 10)
 # Step 1
+driver.get('http://practice.automationtesting.in/')
 # Step 2
+My_Account_Menu = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'My Account')))
+My_Account_Menu.click()
+Login_Email_Text_Field = wait.until(EC.element_to_be_clickable((By.ID, 'username')))
+Login_Email_Text_Field.send_keys('zinkevichva@gmail.com')
+Login_Password_Text_Field = wait.until(EC.element_to_be_clickable((By.ID, 'password')))
+Login_Password_Text_Field.send_keys('1qaz"WSX3edc$RFV')
+Login_Btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'p:nth-child(3) > .button')))
+Login_Btn.click()
 # Step 3
+Menu_Shop = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Shop')))
+Menu_Shop.click()
 # Step 4
+driver.execute_script('window.scrollBy(0, 100);')
+Add_To_Cart_Btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ul li:nth-child(4) a.button')))
+Add_To_Cart_Btn.click()
+time.sleep(1)
+Goods_In_Cart = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a > span.cartcontents')))
+Goods_In_Cart_Value = Goods_In_Cart.text
+assert Goods_In_Cart_Value == '1 Item'
+print('In the cart is', Goods_In_Cart_Value + '.', end = '')
+print('Test passed.')
+Goods_Price_In_Cart = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a > span.amount')))
+Goods_Price_In_Cart_Value = Goods_Price_In_Cart.text
+assert Goods_Price_In_Cart_Value == '₹180.00'
+print('Goods in the cart is for', Goods_Price_In_Cart_Value + '.', end = '')
+print('Test passed.')
 # Step 5
+Shopping_Cart_Ico = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#wpmenucartli > a > i')))
+Shopping_Cart_Ico.click()
 # Step 6
+Subtotal_Value = wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'tr.cart-subtotal > td > span'), '₹'))
 # Step 7
+Total_Value = wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'tr.order-total > td > strong > span'), '₹'))
+# Cleaning shopping cart
+Remove_Item_From_Cart = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'td.product-remove > a')))
+Remove_Item_From_Cart.click()
 
+driver.quit()
+'''
 
 #Tast 9: Work in basket
+'''
+import time
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(5)
+wait = WebDriverWait(driver, 10)
 # Step 1
+driver.get('http://practice.automationtesting.in/')
 # Step 2
+Menu_Shop = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Shop')))
+Menu_Shop.click()
 # Step 3
-# Step 4
-# Step 5
+driver.execute_script('window.scrollBy(0, 300);')
+Add_To_Cart_Btn_HTML = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ul li:nth-child(4) a.button')))
+Add_To_Cart_Btn_HTML.click()
+time.sleep(1)
+Add_To_Cart_Btn_JS = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ul li:nth-child(5) a.button')))
+Add_To_Cart_Btn_JS.click()
+Goods_In_Cart = wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'a > span.cartcontents'), '2 Items'))
+# # Step 4
+Shopping_Cart_Ico = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#wpmenucartli > a > i')))
+Shopping_Cart_Ico.click()
+# # Step 5
+time.sleep(1)
+Remove_Item_From_Cart = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'tr:nth-child(1) > td.product-remove > a')))
+Remove_Item_From_Cart.click()
 # Step 6
+Undo_Link = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Undo?')))
+Undo_Link.click()
 # Step 7
+Quantity_Text_Field = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'tr:nth-child(2) > td.product-quantity > div > input')))
+Quantity_Text_Field.clear()
+Quantity_Text_Field.send_keys('3')
 # Step 8
+Update_Basket = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'tr:nth-child(3) > td > input.button')))
+Update_Basket.click()
 # Step 9
+Quantity_Text_Field_Value = Quantity_Text_Field.get_attribute('value')
+assert Quantity_Text_Field_Value == '3'
+print('Test passed. In quantity text field equal is', Quantity_Text_Field_Value + '.')
 # Step 10
+time.sleep(1)
+Apply_Coupon_Btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'tr:nth-child(3) > td > div > input.button')))
+Apply_Coupon_Btn.click()
 # Step 11
+Alert_Messege_Field= wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'div.woocommerce > ul > li'), 'Please enter a coupon code.'))
 
+time.sleep(1)
+driver.quit()
+'''
 #Tast 10: Purchase of goods
 
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(5)
+wait = WebDriverWait(driver, 10)
 # Step 1
+driver.get('http://practice.automationtesting.in/')
 # Step 2
+Menu_Shop = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Shop')))
+Menu_Shop.click()
 # Step 3
+driver.execute_script('window.scrollBy(0, 300);')
+Add_To_Cart_Btn_HTML = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ul li:nth-child(4) a.button')))
+Add_To_Cart_Btn_HTML.click()
+time.sleep(1)
 # Step 4
+Shopping_Cart_Ico = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#wpmenucartli > a > i')))
+Shopping_Cart_Ico.click()
 # Step 5
+Proceed_to_Checkout_Btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.woocommerce > div > div > div > a')))
+Proceed_to_Checkout_Btn.click()
 # Step 6
+First_Name_Field = wait.until(EC.element_to_be_clickable((By.ID, 'billing_first_name')))
+First_Name_Field.send_keys('Vadim')
+Last_Name_Field = wait.until(EC.element_to_be_clickable((By.ID, 'billing_last_name')))
+Last_Name_Field.send_keys('Zinkevich')
+Email_Address_Field = wait.until(EC.element_to_be_clickable((By.ID, 'billing_email')))
+Email_Address_Field.send_keys('zinkevichva@gmail.com')
+Phone_Field = wait.until(EC.element_to_be_clickable((By.ID, 'billing_phone')))
+Phone_Field.send_keys('1234567890')
+driver.execute_script('window.scrollBy(0, 500);')
+Country_Selector = wait.until(EC.element_to_be_clickable((By.ID, 'select2-chosen-1')))
+Country_Selector.click()
+Country_Selector_Search = wait.until(EC.element_to_be_clickable((By.ID, 's2id_autogen1_search')))
+Country_Selector_Search.send_keys('Russia')
+Country_Selector_Search_Result = wait.until(EC.element_to_be_clickable((By.ID, 'select2-results-1')))
+Country_Selector_Search_Result.click()
+Address_Field = wait.until(EC.element_to_be_clickable((By.ID, 'billing_address_1')))
+Address_Field.send_keys('Birzhevoy bridge')
+Town_City_Field = wait.until(EC.element_to_be_clickable((By.ID, 'billing_city')))
+Town_City_Field.send_keys('Saint-Peterburg')
+State_City_Field = wait.until(EC.element_to_be_clickable((By.ID, 'billing_state')))
+State_City_Field.send_keys('Leningrad region')
+Postcode_Field = wait.until(EC.element_to_be_clickable((By.ID, 'billing_postcode')))
+Postcode_Field.send_keys('1234567')
 # Step 7
+driver.execute_script('window.scrollBy(0, 600);')
+Check_Payments_Radiobutton = wait.until(EC.element_to_be_clickable((By.ID, 'payment_method_cheque')))
+Check_Payments_Radiobutton.click()
 # Step 8
+Place_Order_Btn = wait.until(EC.element_to_be_clickable((By.ID, 'place_order')))
+Place_Order_Btn.click()
 # Step 9
+Thanks_Messege_Field = wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'p.woocommerce-thankyou-order-received'), 'Thank you. Your order has been received.'))
 # Step 10
+Payment_Method_Messege_Field = wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'li.method > strong'), 'Check Payments'))
+
+time.sleep(1)
+driver.quit()
